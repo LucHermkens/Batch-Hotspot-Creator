e27f26fb980b1faad70b6bae662715700d72bb0e
ElektroPyro <hermkensluc@gmail.com>
2016-03-20 19:52:29 +0100

    Updated automatic Administrator privileges

--- source/WiFi Hotspot Creator.bat
+++ source/WiFi Hotspot Creator.bat
@@ -6,6 +6,7 @@ if '%errorlevel%' NEQ '0' (
 goto UACPrompt
 ) else ( goto gotAdmin )
 :UACPrompt
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 "%temp%\getadmin.vbs"
